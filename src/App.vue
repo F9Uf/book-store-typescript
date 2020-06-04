5f8fa0c6198bd1ad7397b9f6d5a73bc61ee378b8
F9Uf <atoseka@gmail.com>
2020-06-04 16:49:02 +0700

    update layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="header" class="flex bg-white border-b border-gray-200 h-16 items-center">
+    <div id="header" class="fixed flex bg-white border-b border-gray-200 h-16 items-center w-full shadow-sm">
       <div id="nav" class="mx-auto px-6 reactive flex items-center w-full">
         <div id="title" class="mr-auto">
           <h1>Book Store</h1>
@@ -11,6 +11,8 @@
         </div>
       </div>
     </div>
-    <router-view />
+    <div id="content" class="pt-20 w-full px-6">
+      <router-view />
+    </div>
   </div>
 </template>
